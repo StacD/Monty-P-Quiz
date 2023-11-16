f5df6de00d2aecc67725c570e50e49b337b3d193
StacD <>
2023-11-16 00:49:39 +0000

    Added questions list

--- run.py
+++ run.py
@@ -5,4 +5,14 @@ print("Welcome to this Monty Python Quiz!\n")
 print("Select A, B or C for your answer then press enter.\n")
 print("First answer only is accepted!\n")
 
-questions = ()
+questions = ("Nobody expects what historic event?\n",
+    "Michael Palin seeks out John Cleese to have what type of conflict?\n",
+    "“He’s not the Messiah, he’s a very naughty boy.” Comes from what film?\n",
+    "Which Monty Python member appears in the Harry Potter films?\n",
+    "The Pythons debuted with what TV show?\n",
+    "The Pythons released what sketch-based movie in 1983?\n",
+    "What sketch about canned meat gave us the nickname for junk email?\n",
+    "What was the third movie done by the Pythons?\n",
+    "What Python musical debuted on Broadway in 2005?\n",
+    "Monty Python & the Holy Grail was shot mostly in what country?\n"
+    )
